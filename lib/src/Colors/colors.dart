d4ea4cda8af063547a8856240979a5760bb835f5
jesusc0310 <jesuscruz@fernando3martos.com>
2020-12-02 14:15:46 +0100

    initial commit

---
+++ lib/src/Colors/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+final azulClarito = Color.fromRGBO(8, 155, 218, 1); //#089BDA
+final azulBase = Color.fromRGBO(42, 87, 165, 1); //#2A57A5
+final blanco = Color.fromRGBO(255, 255, 255, 1); //#FFFFFF
+final negro = Color.fromRGBO(0, 0, 0, 1); //#000000
+final azulOscuro = Color.fromRGBO(36, 84, 154, 1); //#24549A
+final rojo = Color.fromRGBO(200, 75, 75, 1); //#C84B4B
+final gris = Color.fromRGBO(237, 237, 237, 1); //#EDEDED
+final amarillo = Color.fromRGBO(255, 237, 61, 1); //#FFED3D
