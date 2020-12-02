4b3d87c4fd3efd5898a76eed5ccbd8d4e921f92a
jesusc0310 <jesuscruz@fernando3martos.com>
2020-12-02 14:29:59 +0100

    Commit 1: created: main.dart, pages/login_page.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iseneca/src/Colors/colors.dart';
+
+import 'src/pages/login_page.dart';
+
+main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.light.copyWith(statusBarColor: azulBase));
+
+    return MaterialApp(
+      title: 'iSeneca',
+      routes: {}, //TODO: routes
+      debugShowCheckedModeBanner: false,
+      //initialRoute: , TODO: initialRoute
+      home: LoginPage(),
+
+      ///[CAMBIAR EL NOMBRE DE LA PAGINA PARA PODER EJECUTAR EL PROGRAMA]
+    );
+  }
+}
